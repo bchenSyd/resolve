d31ef5c41ba8883a37feaebffdb58b21c047a469
Bo Chen <bochen2014@yahoo.com>
2021-03-09 16:00:22 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MiniFormik extends React.Component {
+  state = {
+    values: this.props.initialValues || {},
+    touched: {},
+    errors: {},
+  };
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    event.persist(); // before react 17, events are pooled and reused (nullified after handled called)
+    this.setState((prevState) => ({
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+    }));
+  };
+  handleBlur = (event) => {
+    const name = event.target.name;
+    event.persist(); // before react 17, events are pooled and reused (nullified after handled called)
+    this.setState((prevState) => ({
+      touched: {
+        ...prevState.touched,
+        [name]: true,
+      },
+    }));
+  };
+  handleSubmit = e=> {
+    e.preventDefault();
+    //validate
+    this.props.onSubmit(this.state.values)
+  }
+  render() {
+    return this.props.children({
+      ...this.state,
+      handleChange: this.handleChange,
+      handleBlur: this.handleBlur,
+      handleSubmit: this.handleSubmit,
+    });
+  }
 }
-
-export default App;
+class Reservation extends React.Component {
+  render() {
+    return (
+      <MiniFormik
+        initialValues={{
+          isGoing: true,
+          numberOfGuests: 2,
+        }}
+        onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+      >
+        {(props) => {
+          const { values, handleChange, handleBlur, errors, touched, handleSubmit } = props;
+          return (
+            <form onSubmit={handleSubmit}>
+              <label>
+                Is going:
+                <input
+                  name="isGoing"
+                  type="checkbox"
+                  checked={values.isGoing}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </label>
+              <br />
+              <label>
+                Number of guests:
+                <input
+                  name="numberOfGuests"
+                  type="number"
+                  value={values.numberOfGuests}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </label>
+              <pre>{JSON.stringify(props, null, 2)}</pre>
+            </form>
+          );
+        }}
+      </MiniFormik>
+    );
+  }
+}
+export default Reservation;
